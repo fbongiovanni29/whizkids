5d91e15b0c6433adbea4f0ca54b04e201b025111
Francis Bongiovanni <fbongiovanni29@gmail.com>
2015-08-15 20:50:21 -0400

    First commit, working product

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+	$(".signup").click(function() {
+		alert("Sign up not available yet!")
+	});	
+		
+        $(".login").click(function() {
+                alert("Login not available yet!")
+        });
+
+
+});
